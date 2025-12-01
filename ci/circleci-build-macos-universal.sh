rgleason/weather_routing_pi

commit 53a9c98f2f9c2976405a269bafdfa5f96f423707

1.15.34.2 + small chngs, fix android build

ci/circleci-build-macos-universal.sh
@@ -64,7 +64,7 @@ cmake \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
   -DOCPN_TARGET_TUPLE="darwin-wx32;10;universal" \
   -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
-  -DOCPN_BUILD_TEST=ON \
+#  -DOCPN_BUILD_TEST=ON \
   ..
 
 if [[ -z "$CI" ]]; then
@@ -73,9 +73,9 @@ if [[ -z "$CI" ]]; then
     exit 0
 fi
 
-make -j 12 # Should ideally be 'make -j $(nproc)' but nproc is not installed in circleci VM
+# make -j 12 # Should ideally be 'make -j $(nproc)' but nproc is not installed in circleci VM
 # See https://man7.org/linux/man-pages/man1/nproc.1.html - we could install GNU coreutils
-make test
+# make test
 # non-reproducible error on first invocation, seemingly tarball-conf-stamp
 # is not created as required.
 make package || make package

.circleci/config.yml.bak
@@ -130,7 +130,7 @@ jobs:
 ## ---------------------
     build-android-arm64:
         docker:
-        - image: cimg/android:2023.12-ndk
+        - image: cimg/android:2025.04-ndk
         resource_class: large
         environment:
         - OCPN_TARGET:  android-arm64
@@ -144,7 +144,7 @@ jobs:
             install_python: true
     build-android-armhf:
         docker:
-        - image: cimg/android:2023.12-ndk
+        - image: cimg/android:2025.04-ndk
         resource_class: large
         environment:
         - OCPN_TARGET:  android-armhf

CMakeLists.txt
@@ -80,7 +80,7 @@ set(VERSION_MAJOR "1")
 set(VERSION_MINOR "15")
 
 set(VERSION_PATCH "34")
-set(VERSION_TWEAK "1")
+set(VERSION_TWEAK "2")
 set(VERSION_DATE "25/11/2025") # DD/MM/YYYY format
 set(OCPN_MIN_VERSION "ov58")
 

bldwin-debug.sh
@@ -1,0 +1,88 @@
+#!/bin/bash
+
+# FE2 Testplugin
+# REKWITHDEBINFO VERSION
+# Use "./build-win.sh" to run cmake.
+# Adjust this command for your setup and Plugin.
+# Requires wxWidgets setup
+# - /home/fcgle/source/ocpn-wxWidgets
+# - /home/fcgle/source/ where all the plugins and OpenCPN repos are kept.
+# --------------------------------------
+# For Opencpn using MS Visual Studio 2022
+# --------------------------------------
+# Used for local builds and testing.
+# Create an empty "[plugin]/build" directory
+# Use Bash Prompt from the [plugin] root directory: "bash ./bldwin-rdeb.sh"
+# Find any errors in the build/output.txt file
+# Then use bash prompt to run cloudsmith-upload.sh command: "bash ./bldwin-rdeb.sh"
+# Which adds the metadata file to the tarball gz file.
+# Set local environment to find and use wxWidgets
+
+# Enable command tracing
+
+set -x 
+
+# Confirm build exists and empty it and if no build directory create it.
+
+if [ -d "build" ]; then
+    echo "The 'build' directory exists, remove all build dir files."
+    rm -rf build/*
+	
+else
+    echo "The 'build' directory does not exist. Create the build directory"
+	mkdir build
+fi
+
+# wxWidgets settings 
+# set "wxDIR=C:\Users\fcgle\source\opencpn\..\ocpn_wxWidgets" 
+# set "wxWIN=C:\Users\fcgle\source\opencpn\..\ocpn_wxWidgets" 
+# set "wxWidgets_ROOT_DIR=C:\Users\fcgle\source\opencpn\..\ocpn_wxWidgets" 
+# set "wxWidgets_LIB_DIR=C:\Users\fcgle\source\opencpn\..\ocpn_wxWidgets\lib\vc_dll" 
+
+# wxWidgets settings 
+C:\Users\fcgle\source\opencpn\cache\buildwin\wxWidgets
+set "wxDIR=C:\Users\fcgle\source\opencpn\cache\buildwin\wxWidgets"
+set "wxWIN=C:\Users\fcgle\source\opencpn\cache\buildwin\wxWidgets"
+set "wxWidgets_ROOT_DIR=C:\Users\fcgle\source\opencpn\cache\buildwin\wxWidgets"
+set "wxWidgets_LIB_DIR=C:\Users\fcgle\source\opencpn\\cache\buildwin\wxWidgets\lib\vc_dll"
+
+set "VCver=17" 
+set "VCstr=Visual Studio 17" 
+
+# wxDIR=$WXWIN
+# wxWidgets_ROOT_DIR=$WXWIN
+# wxWidgets_LIB_DIR="$WXWIN/lib/vc14x_dll"
+# WXWIN="/home/fcgle/source/wxWidgets-3.2.2"
+
+# build the plugin with cmake
+
+cd build
+cmake -T v143 -A Win32 -DOCPN_TARGET=MSVC ..
+cmake --build . --target package --config Debug >output.txt 2>&1
+
+	
+# Bash script completes tarball prep adding metadata into it.
+
+bash ./cloudsmith-upload.sh
+
+# Find ${bold}"build/output.txt"${normal} file if the build is not successful.
+# Other examples below.
+
+# Copy .dll and .pdb files for debugging into MSVisualStudio Development Setup
+# Copy from 
+# C:\Users\fcgle\source\weather_routing_pi\build\relwithdebinfo   weather_routing_pi.dll and weather_routing_pi.pdb
+# into
+# C:\Users\fcgle\source\opencpn\build\RelWithDebInfo\plugins
+
+
+# cp -rv ./SourceFolder ./DestFolder
+# cp -r ./dist/* ./out
+#    -r - Copy all files and folders inside a directory
+#    -i - Ask before replacing files
+#    -u - Copy only if the source is newer
+#    -v - Verbose mode, show files being copied
+# copy ..\build\relwithdebinfo\weather_routing_pi.dll to  C:\Users\fcgle\source\opencpn\build\RelWithDebInfo\plugins
+# copy ..\build\relwithdebinfo\weather_routing_pi.pdb to  C:\Users\fcgle\source\opencpn\build\RelWithDebInfo\plugins
+
+cp -uv ./Debug/*_pi.dll C:/Users/fcgle/source/opencpn/build/Debug/plugins
+cp -uv ./Debug/*_pi.pdb C:/Users/fcgle/source/opencpn/build/Debug/plugins

ci/circleci-build-macos-universal.sh.bak
@@ -64,6 +64,7 @@ cmake \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
   -DOCPN_TARGET_TUPLE="darwin-wx32;10;universal" \
   -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+  -DOCPN_BUILD_TEST=ON \
   ..
 
 if [[ -z "$CI" ]]; then
@@ -72,13 +73,12 @@ if [[ -z "$CI" ]]; then
     exit 0
 fi
 
-
-
-
-
-#make -j$(nproc) 
-#make test
-#make package
+make -j 12 # Should ideally be 'make -j $(nproc)' but nproc is not installed in circleci VM
+# See https://man7.org/linux/man-pages/man1/nproc.1.html - we could install GNU coreutils
+make test
+# non-reproducible error on first invocation, seemingly tarball-conf-stamp
+# is not created as required.
+make package || make package
 
 # Create the cached /usr/local archive
 if [ -n "$CI"  ]; then

cmake/in-files/cloudsmith-upload.sh.in.bak
@@ -58,13 +58,6 @@ if [ -z "$CLOUDSMITH_API_KEY" ] && [ "$LOCAL_BUILD" = "false" ]; then
     echo 'Cannot deploy to cloudsmith, missing $CLOUDSMITH_API_KEY'
     exit 0
 fi
-if [ -z "$CIRCLE_USERNAME" ]; then
-  echo "CircleCI username not set. Defaulting to 'unknown'."
-  CIRCLE_USERNAME="unknown"
-fi
-
-echo "Uploading as $CIRCLE_USERNAME..."
-# Proceed with upload logic
 set -x
 
 if [ "$CIRCLECI" ] || [ "$TRAVIS" ]; then

commit d9e5e8800188f6d3017691016ded98aca6edb4c1

Fix breakage to macos build introduced by https://github.com/rgleason/weather_routing_pi/commit/53a9c98f2f9c2976405a269bafdfa5f96f423707

ci/circleci-build-macos-universal.sh
@@ -64,7 +64,7 @@ cmake \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
   -DOCPN_TARGET_TUPLE="darwin-wx32;10;universal" \
   -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
-#  -DOCPN_BUILD_TEST=ON \
+  -DOCPN_BUILD_TEST=ON \
   ..
 
 if [[ -z "$CI" ]]; then
@@ -73,9 +73,9 @@ if [[ -z "$CI" ]]; then
     exit 0
 fi
 
-# make -j 12 # Should ideally be 'make -j $(nproc)' but nproc is not installed in circleci VM
+make -j 12 # Should ideally be 'make -j $(nproc)' but nproc is not installed in circleci VM
 # See https://man7.org/linux/man-pages/man1/nproc.1.html - we could install GNU coreutils
-# make test
+make test
 # non-reproducible error on first invocation, seemingly tarball-conf-stamp
 # is not created as required.
 make package || make package